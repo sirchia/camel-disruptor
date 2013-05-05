af224c5a773fd80d5dacc8e767126c23db3e593e
Riccardo Sirchia <sirchia.r@gmail.com>
2013-05-05 11:55:35 +0200

    All packages renamed from github domain to apache domain

---
+++ src/main/java/org/apache/camel/component/disruptor/ExchangeEvent.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 Riccardo Sirchia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.component.disruptor;
+
+import org.apache.camel.Exchange;
+
+/**
+ * This is a mutable reference to an {@link Exchange}, used as contents of the Disruptors ringbuffer
+ */
+public class ExchangeEvent {
+
+    private Exchange exchange;
+
+    public Exchange getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(final Exchange exchange) {
+        this.exchange = exchange;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeEvent{" +
+                "exchange=" + exchange +
+                '}';
+    }
+}
